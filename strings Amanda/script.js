7c98e45465cb9ba400283d9a7d671903eecd1af6
valcarenghi <valcarenghi.amanda@escola.pr.gov.br>
2025-10-02 16:13:58 -0300

    Add files via upload

---
+++ strings Amanda/script.js
@@ -0,0 +1,9 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+}
+   
